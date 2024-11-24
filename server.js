6980ff48b470cd974b3f8f30a75af48995ef618a
Lais Aparecida Borges <86136694+laisab@users.noreply.github.com>
2024-11-24 18:29:06 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+// Importa as dependências necessárias
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+
+// Cria uma instância do servidor Express
+const app = express();
+app.use(express.static("uploads"));
+routes(app);
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
